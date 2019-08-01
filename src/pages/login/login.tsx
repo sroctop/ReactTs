dd5fd8b97896beac99271e98f977805237de5ebe
sundapeng <sundapeng@haier.com>
2019-08-01 19:08:31 +0800

    表单验证

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,23 +1,44 @@
-import React, { Component } from 'react'
-import {
-  Form,
-  Icon,
-  Input,
-  Button,
-  Checkbox
-} from 'antd';
+import React, { Component } from "react";
+import { Form, Icon, Input, Button } from "antd";
 
-import './login.less'
-import logo from './images/logo.png';
+import "./login.less";
+import logo from "./images/logo.png";
 
 class Login extends Component {
+  // Form submit function
+  handleSubmit = (event: any) => {
+    event.preventDefault(); // 禁止默认事件
+
+    (this.props as any).form.validateFields((err: any, values: any) => {
+      if (!err) {
+        console.log("提交登录的ajax的请求", values);
+      } else {
+        console.log("校验失败");
+      }
+    });
+  };
+
+  /**
+   * 对密码进行自定义验证
+   */
+  validatorPwd = (rule: any, value: any, callback: any) => {
+    if (!value) {
+      callback("密码必须输入");
+    } else if (value.length < 4) {
+      callback("密码长度不能小于4位");
+    } else if (value.length > 12) {
+      callback("密码长度不能大于12位");
+    } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+      callback("用户名必须是英文数字下划线开头");
+    }
+    callback();
+  };
 
   render() {
-
     // const form = this.props.form;
 
     const form = (this.props as any).form;
-    
+
     const { getFieldDecorator } = form;
     return (
       <div className="login">
@@ -29,47 +50,58 @@ class Login extends Component {
           <h2>用户登录</h2>
           <Form onSubmit={this.handleSubmit} className="login-form">
             <Form.Item>
-              {
-                getFieldDecorator('username', {})(
-                  <Input
-                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    placeholder="Username"
-                  />
-                )
-              }
+              {getFieldDecorator("username", {
+                rules: [
+                  { required: true, whitespace: true, message: "请输入用户名" },
+                  { min: 4, message: "用户名至少4位" },
+                  { max: 12, message: "用户名最多12位" },
+                  {
+                    pattern: /^[a-zA-Z0-9_]+$/,
+                    message: "用户名必须是英文数字下划线开头"
+                  }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="Username"
+                />
+              )}
             </Form.Item>
             <Form.Item>
-              {
-                getFieldDecorator('password', {})(
-                  <Input
-                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    type="password"
-                    placeholder="Password"
-                  />
-                )
-              }
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    validator: this.validatorPwd
+                  }
+                ]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  type="password"
+                  placeholder="Password"
+                />
+              )}
             </Form.Item>
             <Form.Item>
-              <Button type="primary" htmlType="submit" className="login-form-button">
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
                 Login
               </Button>
             </Form.Item>
           </Form>
         </section>
       </div>
-    )
-  }
-
-  // Form submit function
-  handleSubmit = (event: any) => {
-
-    event.preventDefault();
-    const form = (this.props as any).form;
-    const values = form.getFieldsValue();
-    console.log('handleSubmit()',values);
+    );
   }
 }
 
-const WrapLogin = Form.create()(Login)
+const WrapLogin = Form.create()(Login);
 
 export default WrapLogin;
